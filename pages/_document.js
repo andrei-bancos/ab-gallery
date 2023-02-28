47a1b6871a249129335106e7df3a35c1238fdc6e
Andrei Bancoș <bancos.andrei.2002@gmail.com>
2023-02-28 16:58:08 +0200

    Added favicon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#111111" />
+        <link rel="icon" sizes="64x64" href="/favicon/favicon-64x64.png" />
+        <link rel="icon" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/favicon/favicon192.png" />
+
+      </Head>
       <body>
         <Main />
         <NextScript />
